61a2dbf8e55e3df9d6fd9c1da2662cf37e8a2e9f
huhu42 <nikki.hu42@gmail.com>
2023-08-23 16:26:04 -0400

    remove

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -38,11 +38,11 @@ def main():
             st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
 
         # Display the podcast guest and their details in a side-by-side layout
-        col3, col4 = st.columns([3, 7])
+        #col3, col4 = st.columns([3, 7])
 
-        with col3:
-            st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest'])
+        #with col3:
+        #    st.subheader("Podcast Guest")
+        #    st.write(podcast_info['podcast_guest'])
 
         #with col4:
             #st.subheader("Podcast Guest Details")
